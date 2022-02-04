926c99a7a95c064a3ed9dec582ca097f182ecb76
andykimchris <andrew.muchiri97@gmail.com>
2022-02-04 20:16:19 +0300

    added container & stack for budget

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import BudgetCard from "./components/BudgetCard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Stack } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
-const App = () => {
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
   return (
-    <div className="App">
-      ergtredr
-    </div>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap={2} className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+
+      <div className="main">
+        <BudgetCard
+          name="Entertainment"
+          max={1000}
+          amount={100}
+          gray
+        ></BudgetCard>
+      </div>
+    </Container>
   );
-}
+};
 
 export default App;
